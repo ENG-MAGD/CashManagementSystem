f1b2b1af22e90fead4669d57a281128a75393d09
Magd Barghod <magdmohammad92@gmail.com>
2026-09-12 18:19:51 +0300

    Add cash box model

---
+++ Models/CashBox.cs
@@ -0,0 +1,11 @@
+﻿namespace 
+    CashManagementSystem.Models;
+
+public class CashBox
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Currency { get; set;  } = string.Empty;
+    public decimal OpeningBalance {  get; set; }
+    public DateTime CreatedDate { get; set; }
+}
